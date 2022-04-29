54d64ebfaac9be3613b113f41d680ebee0b0e546
sseong <sseong@deepnoid.com>
2022-04-29 09:47:27 +0900

    유효한 팰린드롬

---
+++ seokwon/level-1/valid-palindrome.js
@@ -0,0 +1,28 @@
+// 앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 팰린드롬이라고 합니다.
+// 문자열이 입력되면 해당 문자열이 팰린드롬이면 "YES", 아니면 “NO"를 출력하는 프로그램을 작성하세요.
+// 단 회문을 검사할 때 알파벳만 가지고 회문을 검사하며, 대소문자를 구분하지 않습니다.
+// 알파벳 이외의 문자들의 무시합니다.
+
+/**
+    정규식을 이용해서 영어만 걸러내준다.
+ */
+
+function solution(str){
+    let answer = true;
+    
+    str = str.toLowerCase().replace(/[^a-z]/g, '');
+    let strLen = str.length;
+     
+    for(let i = 0; i < strLen; i++){
+        
+        if(str[i] !== str[strLen - 1 -i ]){
+            answer = false;
+            break;
+        }
+     }
+     return answer ? 'YES' : 'NO';
+}
+
+
+let result = solution('found7, time: study; Yduts; emit, 7Dnuof');
+console.log(result);
