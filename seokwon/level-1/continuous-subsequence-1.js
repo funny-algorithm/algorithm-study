a4ed8e4d0f9f9e73e2e2b1a401acd7312589492b
sseong <sseong@deepnoid.com>
2022-05-04 16:42:24 +0900

    부분 수열 1

--- seokwon/level-1/continuous-subsequence-1.js
+++ seokwon/level-1/continuous-subsequence-1.js
@@ -1,25 +1,38 @@
 function solution(N, M, arr) {
     let answer = 0;
     let end = 0;
+    
+    let sum = 0;
+    // start pointer = i
     for(let i = 0; i < N; i++){
-        let sum = arr[i];
-        
         while(end < N){
-            if(i !== end){
+            // 두가지 포인터가 같아지면 합을 구할 필요가 없음
+            if(i === end){
+                sum = arr[i];
+            //아닐 경우에는 end를 누적 시키면서 나아간다
+            }else{
                 sum += arr[end];
             }
+            
             if(sum < M){
                 end++;
-            }else if(sum === N){
+            }else if(sum === M){
                 answer++;
+                sum -= arr[end];
+                sum -= arr[i];
+                break;
+            }else if(sum > M){
+                sum -= arr[end];
+                sum -= arr[i]
                 break;
             }
         }
+        
     }
     return answer;
 }
 
 
 
-let res = solution(8,6, [2,2,1,3,1,1,1,2]);
+let res = solution(8,6, [1,2,1,3,1,1,1,2]);
 console.log(res);
