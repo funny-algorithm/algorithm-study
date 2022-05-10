e81829c92fd16e248ce9900114174f46d3a36e0e
sseong <sseong@deepnoid.com>
2022-05-10 13:16:26 +0900

    풀이 추가

--- seokwon/level-1/combine-two-arrays.js
+++ seokwon/level-1/combine-two-arrays.js
@@ -8,6 +8,38 @@ function solution(arr1, arr2){
     return arr1.concat(arr2).sort((a, b) => a - b);
 }
 
+function solution2(arr1, arr2){
+    let answer = [];
+    let n = arr1.length;
+    let m = arr2.length;
 
+    let p1 = p2 = 0;
+
+    while(p1 < n && p2 < m){
+        if(arr1[p1] <= arr2[p2]){
+            answer.push(arr1[p1++]);
+        }else{
+            answer.push(arr2[p2++]);
+        }
+    }
+
+    while(p1 < n) {
+        answer.push(arr1[p1++]);
+    }
+
+    while(p2 < m ){
+        answer.push(arr2[p2++]);
+    }
+    return answer;
+}
+
+
+console.time('res1');
 let res = solution([1,3,5], [2,3,6,7,9]);
 console.log(res);
+console.timeEnd('res1');
+
+console.time('res2');
+let res2 = solution2([1,3,5], [2,3,6,7,9]);
+console.log(res2);
+console.timeEnd('res2');
--- seokwon/level-1/finding-common-elements.js
+++ seokwon/level-1/finding-common-elements.js
@@ -8,6 +8,38 @@ function solution(arr1, arr2){
     return arr1.filter(el => arr2.includes(el)).sort((a, b) => a - b);
 }
 
+function solution2(arr1, arr2){
+    arr1.sort((a, b) => a - b);
+    arr2.sort((a, b) => a - b);
 
+    let answer = [];
+    let n = arr1.length;
+    let m = arr2.length;
+    let p1 = p2 =0;
+
+    while(p1 < n && p2 < m){
+        if(arr1[p1] === arr2[p2]){
+            answer.push(arr1[p1])
+            p1++;
+            p2++;
+        }else{
+            if(arr1[p1] < arr2[p2]){
+                p1++;
+            }else{
+                p2++;
+            }
+        }
+    }
+    return answer;
+
+}
+
+console.time('res1');
 let res = solution([1,3,9,5,2], [3,2,5,7,8]);
 console.log(res);
+console.timeEnd('res1');
+
+console.time('res2');
+let res2 = solution2([1,3,9,5,2], [3,2,5,7,8]);
+console.log(res2);
+console.timeEnd('res2');
