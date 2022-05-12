45f32e7112ac6563fe0f61b29de238fa69b3309e
Seongseokwon <pam9411@naver.com>
2022-05-12 20:53:56 +0900

    anagram 완성

--- seokwon/level-1/anagram.js
+++ seokwon/level-1/anagram.js
@@ -2,8 +2,7 @@ function solution(str1, str2){
     let store1 = new Map();
     let store2 = new Map();
     
-
-    // 문자열 순회하면서 
+    // 문자열 순회하면서  Map을 만들어 준다.
     for(let i = 0; i < str1.length; i++){
         if(store1.has(str1[i])){
             store1.set(str1[i], store1.get(str1[i]) + 1);
@@ -12,13 +11,27 @@ function solution(str1, str2){
         }
 
         if(store2.has(str2[i])){
-            store2.set(str1[i], store2.get(str2[i]) + 1);
+            store2.set(str2[i], store2.get(str2[i]) + 1);
         }else{
             store2.set(str2[i], 1);
         }
     }
-
     
+    //만들어진 Map을 순회하면서 값을 비교해준다.
+    let check = true;
+    for(const [key, value] of store1){
+        if(store2.has(key)){
+            if(store2.get(key) !== value){
+                check = false;
+                break;
+            }
+        }else {
+            check =false;
+            break;
+        }
+    }
+    return check ? 'YES' : 'NO';
 }
 
-let res = solution('AbaAeCe', 'baeeACA');
+let res = solution('abaCC', 'Caaab');
+console.log(res);
